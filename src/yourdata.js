6dd488cdca3811356d7b44c9ec5890cf6b275d4c
YNotes <65722992+YNseries@users.noreply.github.com>
2021-06-14 16:23:06 +0900

    upload data

---
+++ src/yourdata.js
@@ -0,0 +1,195 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import javaIcon from "./images/java.svg"
+import oracleIcon from "./images/oracle.svg"
+import jsIcon from "./images/javascript.svg"
+import jspIcon from "./images/jsp.svg"
+import pythonIcon from "./images/python.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "서유나",
+  headerTagline: [
+    //Line 1 For Header
+    "안녕하세요.",
+    //Line 2 For Header
+    "신입 개발자",
+    //Line 3 For Header
+    "서유나입니다.",
+  ],
+  //   Header Paragraph
+  // headerParagraph:
+  //   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+
+  //Contact Email
+  contactEmail: "ynotes4@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "냉장고 자료로 레시피 추천", //Project Title - Add Your Project Title Here
+      para:
+        "음식 콘텐츠 소비 빈도의 증가로 실제 본인이 가진 재료를 기반으로 레시피 검색, 찜 기능을 이용해서 관련된 상품을 구매할 수 있도록 돕는 사이트 제작", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../images/thumbnail_1.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://youtu.be/EUWeDyiKtIk",
+    },
+    {
+      title: "콕방 운동", //Project Title - Add Your Project Title Here
+      para:
+        "헬스장 등 외부로 운동하러 가기 힘들어진 상황에 집에서 운동을 즐기는 사람들의 증가로 이를 좀 더 효율적으로 할 수 있게 관리를 도와주는 사이트를 구현하기로 함", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "#",
+      //Project URL - Add Your Project Url Here
+      url: "#",
+    },
+    {
+      title: "Django Project", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    // {
+    //   title: "Project Four", //Project Title - Add Your Project Title Here
+    //   para:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+    // {
+    //   title: "Project Five", //Project Title - Add Your Project Title Here
+    //   para:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+    // {
+    //   title: "Project Six", //Project Title - Add Your Project Title Here
+    //   para:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+    //   //Project Image - Add Your Project Image Here
+    //   imageSrc:
+    //     "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+    //   //Project URL - Add Your Project Url Here
+    //   url: "http://chetanverma.com/",
+    // },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "워킹 홀리데이를 들어가기 전에는 학생들을 대상으로 영어교육 강의 안내와 디자인 광고물 제작 등의 일을 한 적이 있고, 워킹 홀리데이를 가서는 스타벅스와 맥도날드 같은 프랜차이즈에서 일을 하였습니다. 이 이들의 공통점은 고객들의 요구를 파악하여 만족할 수 있는 서비스를 제공한다는 것입니다. 프로그래밍 또한 크게 다르지 않다고 생각합니다. 프로그래밍하는 사람이 아무리 좋다고 이야기를 해도 고객이 만족하지 않는다면 그 프로그래밍은 의미가 없다고 생각합니다. 그런 점에서 저의 경험들이 고객 만족 서비스를 끌어낼 수 있는 기본적인 밑바탕이었다고 생각합니다. 실제로 프로젝트를 진행하면서 발표용 PPT나 웹 사이트용 템플릿을 찾을 때 주도적으로 나서 제시를 하고 팀원들의 선택을 받을 수 있었습니다.",
+  // aboutParaTwo:
+  //   "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // aboutParaThree:
+  //   "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  aboutImage:
+    "#",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: javaIcon,
+      para:
+        "기본적인 콘솔 입/출력부터 배열 및 객체지향 프로그래밍을 배웠으며, 입출력 파일과 Network System을 이용한 게시판 제작 등 기본적인 기능 구현 가능함",
+    },
+    {
+      img: oracleIcon,
+      para:
+        "DBMS와 오라클의 개념, 데이터베이스 객체, PL/SQL, 고급 SQL 및 MySql 데이터베이스 개념을 배움. 또한, 마이바티스 웹 애플리케이션과 활용 / 마이바티스와 스프링 웹 애플리케이션 연동을 해 봄",
+    },
+    {
+      img: jspIcon,
+      para:
+        "웹 프로그래밍, JSP를 토대로 이클립스를 이용한 웹 개발을 해 봄. 실제 활용을 위해 jsoup을 이용한 크롤링을 시도해 보고, chartjs를 이용한 데이터 시각화를 배움",
+    },
+    {
+      img: jsIcon,
+      para:
+        "객체지향 자바스크립트의 이해 / 웹사이트의 동적인 구조 및 요소를 추가해 봄. 또, jQuery(CSS 셀렉터, 브라우저의 호환성, 플러그인 플래시 콘텐츠를 대체)를 배움",
+    },
+    {
+      img: htmlIcon,
+      para:
+        "Html 과 CSS, CSS구성과 기본사용법을 익힘",
+    },
+    {
+      img: pythonIcon,
+      para:
+        "Java와 Python의 융합과정으로서 스프링에서 돌아가는 사이트에서 활용되는 데이터를 가져와 Django에서 구현해 봄",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "5.5개월이라는 집중된 시간 동안 이론만이 아닌 실무에서 쓰이는 교육 몰입해서 배웠기 때문에 실무에 좀 더 빠르게 적응할 수 있을 거로 생각합니다. 어느 직종이든 최소 1년 동안은 업무에 대한 적응과 함께 업무를 바라보는 시각을 배우는 시간이라고 생각합니다. 그 후부터는 1년 동안 익힌 업무패턴을 토대로 실무를 좀 더 익혀가는 시간이라고 생각하기에 프로그래밍에 대하여 저만의 길을 만들어갈 것입니다. 이후 3년 차부터는 프로그래밍에 대한 저의 역량이 부족하다고 생각이 되면 추가적인 공부를 통하여 업무에 대한 저의 역량을 좀 더 키워나가겠습니다.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "감사합니다",
+  // social: [
+  //   // Add Or Remove The Link Accordingly
+  //   { img: githubIcon, url: "https://github.com/chetanverma16" },
+  //   {
+  //     img: codepenIcon,
+  //     url: "https://www.codepen.com/",
+  //   },
+  //   {
+  //     img: dribbbleIcon,
+  //     url: "https://dribbble.com/chetanverma",
+  //   },
+  //   {
+  //     img: instagramIcon,
+  //     url: "https://www.instagram.com/",
+  //   },
+  // ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
